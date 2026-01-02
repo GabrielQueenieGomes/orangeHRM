8b908486f2bfc12bd1511a1e67cdfc267e800f7e
gabrielqueeniegomes <queeniegomes93@gmail.com>
2026-01-02 16:34:20 -0500

    refactored to test folders

---
+++ src/test/java/com/runner/Runner.java
@@ -0,0 +1,15 @@
+package com.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+	    features = "src/test/resources/login.feature",
+	    glue = "com.cucumber.bdd.login",
+	    plugin = { "json:target/cucumber.json" },
+	    monochrome = true
+	)
+
+public class Runner extends AbstractTestNGCucumberTests {
+
+}
